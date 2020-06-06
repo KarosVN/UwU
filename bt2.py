6443a2c17ffa08fd40871c29165aa085874b7bfd
Vĩnh Toàn <66415025+KarosVN@users.noreply.github.com>
2020-06-06 08:13:21 +0700

    Update bt2.py

--- bt2.py
+++ bt2.py
@@ -5,5 +5,7 @@ if (a > 0):
         t *= i + 1;
     print('giai thua cua {0} la {1}'.format(a,t));
 else:
-    print('Moi nhap lai');
+    for i in range(0, a):
+        t *= i - 1
+    print('giai thua cua {0} la {1}'.format(a,t));
 print();
